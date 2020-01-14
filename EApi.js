035ae1a59d7bd470b74dff2038f0a2096faa76fa
arily <lbsaaaaa@gmail.com>
2020-01-15 00:46:43 +0900

    Update EApi.js

--- EApi.js
+++ EApi.js
@@ -16,7 +16,7 @@ async function checkStatus(res, json) {
 
     if (res.ok) { // res.status >= 200 && res.status < 300
         if (res.status == 202) {
-            if (code !== 40004) throw new EndpointServerSideError(json);
+            if (json.code !== 40004) throw new EndpointServerSideError(json);
         } else {
             if (50000 > json.code && json.code >= 40000 && code !== 40004) throw new EndpointServerSideError(json);
         }
